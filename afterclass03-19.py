2b183ada52540aa0e314089ecaacc1ea87224c1e
Kwiecinska-Karolina <92158312+Kwiecinska-Karolina@users.noreply.github.com>
2021-11-23 17:29:18 +0100

    Update afterclass03-19.py

--- afterclass03-19.py
+++ afterclass03-19.py
@@ -1,3 +1,8 @@
+""" Write a program that calculates a dog's age in dog’s years.
+For the first two years, a dog's life is equal to 10.5 human years. 
+After that, each dog year is equal to 4 human years. Sample result:
+
+Enter the dog's age in human years: 15 The dog's age in dog’s years is 73 years."""
 age = float(input("Podaj wiek psa: "))
 rok = 10.5
 dwa_lata = rok * 2
