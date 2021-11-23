06e0e6fe984a93485d2376b3334dae5171276c68
Kwiecinska-Karolina <92158312+Kwiecinska-Karolina@users.noreply.github.com>
2021-11-23 17:27:19 +0100

    Update afterclass03-16.py

--- afterclass03-16.py
+++ afterclass03-16.py
@@ -1,3 +1,6 @@
+"""Write a program that displays two numbers entered from the keyboard in ascending order.
+
+Enter first number: 27 Enter second number: 14 Numbers in ascending order: 14, 27"""
 number_1 = input("Podaj pierwszą liczbę: ")
 number_2 = input("Podaj drugą liczbę: ")
 
