05cf0a06262a80b55ede3d33e90fdcc619d95ebb
Kwiecinska-Karolina <92158312+Kwiecinska-Karolina@users.noreply.github.com>
2021-11-23 17:25:32 +0100

    Update afterclass02-22.py

--- afterclass02-22.py
+++ afterclass02-22.py
@@ -1,3 +1,7 @@
+""" Write a program that enables the user to face the computer. 
+The computer throws a dice. 
+The user then tries to guess the number from a dice by entering a number from 1 to 6 from the keyboard. 
+If the user has guessed the number from the dice, the computer displays True."""
 import random
 
 rzut = random.randint(1,6)
