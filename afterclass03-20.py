1c4a1342be44217f476be7323bfafc46847b60a2
Kwiecinska-Karolina <92158312+Kwiecinska-Karolina@users.noreply.github.com>
2021-11-23 17:30:25 +0100

    Update afterclass03-20.py

--- afterclass03-20.py
+++ afterclass03-20.py
@@ -1,3 +1,18 @@
+"""Write a program that creates a multiplication table in the range 1 to 10 
+for any number entered by the user. 
+Sample result:
+
+Enter number:6 
+6 x 1 = 6 
+6 x 2 = 12 
+6 x 3 = 18 
+6 x 4 = 24 
+6 x 5 = 30 
+6 x 6 = 36 
+6 x 7 = 42 
+6 x 8 = 48 
+6 x 9 = 54 
+6 x 10 = 60"""
 number = int(input("Podaj liczbę: "))
 
 tab= [1,2,3,4,5,6,7,8,9,10]
