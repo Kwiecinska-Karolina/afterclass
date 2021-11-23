e9ba7b0ed6c8d3fe2475792566e8bdaf7cb447d0
Kwiecinska-Karolina <92158312+Kwiecinska-Karolina@users.noreply.github.com>
2021-11-23 17:34:15 +0100

    Update afterclass03-25.py

--- afterclass03-25.py
+++ afterclass03-25.py
@@ -1,3 +1,12 @@
+"""The variables a and b contain the dimensions of the sides of the rectangle.
+Write a program that creates the following rectangle with dimensions a and b. 
+Example result for a = 4 and b = 15:
+
+***************
+*             * 
+*             * 
+***************
+"""
 a = 4
 b = 15
 i = 0
