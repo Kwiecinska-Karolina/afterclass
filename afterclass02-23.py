484a074f289f829b0b040fa6e900c0f3e27e8006
Kwiecinska-Karolina <92158312+Kwiecinska-Karolina@users.noreply.github.com>
2021-11-23 17:42:42 +0100

    Update afterclass02-23.py

--- afterclass02-23.py
+++ afterclass02-23.py
@@ -1,8 +1,3 @@
-""" 23% VAT was paid from the amount of PLN 15.84.
-Calculate and display VAT. 
-Apply formatting with decimal places. Sample result:
-
-Amount : 15.84 zł VAT 23% : 3.64 zł"""
 kwota = float(input())
 stawka_VAT = 0.23
 kwota_VAT = kwota * stawka_VAT
