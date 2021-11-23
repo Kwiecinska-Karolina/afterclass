c8498797cd4f8506e9973329ed3ae6c5102ade35
Kwiecinska-Karolina <92158312+Kwiecinska-Karolina@users.noreply.github.com>
2021-11-23 17:43:52 +0100

    Update afterclass03-28.py

--- afterclass03-28.py
+++ afterclass03-28.py
@@ -1,9 +1,3 @@
-"""Write a program that displays the first fifty words of the Fibonacci sequence.
-The sequence is defined as follows: 
-the first term is equal to 0, the second is equal to 1, each subsequent term is the sum of the previous two.
-Sample result below.
-
-0 1 1 2 3 5 8 13 21 34 ... """
 def fib (n):
     a=0
     b=1
