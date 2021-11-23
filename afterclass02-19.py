429f69127c2d7de74d44580aee2794460a2151f5
Kwiecinska-Karolina <92158312+Kwiecinska-Karolina@users.noreply.github.com>
2021-11-23 17:23:25 +0100

    Update afterclass02-19.py

--- afterclass02-19.py
+++ afterclass02-19.py
@@ -1,3 +1,8 @@
+""" The length of the sides of the triangle is a, b and c.
+Write a program that calculates the area of the triangle using the Heron formula. 
+Read the values of the sides of the triangle from the keyboard. 
+Using the program, calculate the area of the triangle for the sides 3, 4 and 5.
+"""
 a = int(input("Wprowadź długość pierwszego boku: "))
 b = int(input("Wprowadź długość drugiego boku: "))
 c = int(input("Wprowadź długość trzeciego boku: "))
