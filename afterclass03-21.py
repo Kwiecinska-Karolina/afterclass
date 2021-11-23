a36e1bc44f0a355d9f7b003c505d791492adc3b3
Kwiecinska-Karolina <92158312+Kwiecinska-Karolina@users.noreply.github.com>
2021-11-23 17:30:54 +0100

    Update afterclass03-21.py

--- afterclass03-21.py
+++ afterclass03-21.py
@@ -1,3 +1,8 @@
+""" The 'university' variable contains the name of university where you study.
+Write a program that displays the contents of the variable with an extra space
+between characters (add a space between each character).
+
+UEK w Krakowie U E K w K r a k o w i e """
 university = "UEK w Krakowie"
 
 for i in university:
