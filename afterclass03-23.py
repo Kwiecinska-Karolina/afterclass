9ccd40aa2783078bbac15f1ec1677146cb84e66b
Kwiecinska-Karolina <92158312+Kwiecinska-Karolina@users.noreply.github.com>
2021-11-23 17:32:19 +0100

    Update afterclass03-23.py

--- afterclass03-23.py
+++ afterclass03-23.py
@@ -1,4 +1,15 @@
+""" Write a program that creates the following pattern.
+Sample result:
 
+1 
+22 
+333 
+4444 
+55555 
+666666 
+7777777 
+88888888 
+999999999"""
 for i in range(0,10):
    for j in range(1,i+1):
        print(i, end='')
